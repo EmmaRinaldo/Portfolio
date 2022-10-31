5ed6d535107e7835ff9c341261e4c4c858e6403e
EmmaRinaldo <56730801+EmmaRinaldo@users.noreply.github.com>
2022-10-31 03:11:48 +0100

    presque

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $mailForm = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mailTo = "emmarinaldo@hotmail.fr";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have a message ".$name".\n\n".$message;
+
+    mail($mailTo, $name, $txt, $headers);
+
+    header("Location: index.html?MessageSent");
+}
+
+
+
+
+?>
